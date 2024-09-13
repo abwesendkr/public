422de6b24325822458630f29dd7b391c4acabca6
abwesendkr <159249009+abwesendkr@users.noreply.github.com>
2024-09-13 18:04:06 +0200

    Update adobereader-install.ps1

--- adobereader-install.ps1
+++ adobereader-install.ps1
@@ -22,7 +22,7 @@ $app = "adobereader" #enter package name from https://community.chocolatey.org/p
 $version = "2024.2.20857" #version
 $installArgumentsstring = "INSTALLSTRING" #set $null if not in use
 $logPath = "C:\ProgramData\Kapsch\ImageBuilding\$($app)"
-$keepchoco = $False
+$keepchoco = $True
 $installFlag = "$($logPath)\$($app)_installed.txt"
 
 ########
