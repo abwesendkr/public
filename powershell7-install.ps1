e83f6627d13d64e69625076752b55a91dffbaa67
abwesendkr <159249009+abwesendkr@users.noreply.github.com>
2024-09-13 15:10:42 +0200

    Update powershell7-install.ps1

--- powershell7-install.ps1
+++ powershell7-install.ps1
@@ -1,6 +1,6 @@
 # Download PowerShell 7 MSI file
 $msiUrl = "https://github.com/PowerShell/PowerShell/releases/download/v7.4.5/PowerShell-7.4.5-win-x64.msi"
-$msiPath = "C:\Temp\PowerShell-7.2.2-win-x64.msi"
+$msiPath = "C:\Temp\PowerShell-7.4.5-win-x64.msi"
 Invoke-WebRequest -Uri $msiUrl -OutFile $msiPath
 
 # Install PowerShell 7 silently
