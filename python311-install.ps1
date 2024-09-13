bd2d18c1f8fd954c2a8a3550c3839396a3ee1ad5
abwesendkr <159249009+abwesendkr@users.noreply.github.com>
2024-09-13 18:05:59 +0200

    Update python311-install.ps1

--- python311-install.ps1
+++ python311-install.ps1
@@ -22,7 +22,7 @@ $app = "python311" #enter package name from https://community.chocolatey.org/pac
 $version = "3.11.9" #version
 $installArgumentsstring = "INSTALLSTRING" #set $null if not in use
 $logPath = "C:\ProgramData\Kapsch\ImageBuilding\$($app)"
-$keepchoco = $False
+$keepchoco = $True
 $installFlag = "$($logPath)\$($app)_installed.txt"
 
 ########
