0fbe57b8c132337a9005bd7b36214b9afe9c4fef
abwesendkr <manchmal2@abwesend.de>
2024-09-30 12:34:47 +0200

    execution policy error

--- install-packagemgr.ps1
+++ install-packagemgr.ps1
@@ -50,7 +50,7 @@ function Install-Wingetpowershell {
             Write-Host "Set-ExecutionPolicy..."
             
             # Setzen der Execution Policy auf RemoteSigned
-            Set-ExecutionPolicy -ExecutionPolicy "RemoteSigned" -Force
+            Set-ExecutionPolicy -ExecutionPolicy "RemoteSigned" -Force -ErrorAction Ignore
             Write-Host "Install-Module..."
             
             # Installieren des Moduls PowerShellGet
