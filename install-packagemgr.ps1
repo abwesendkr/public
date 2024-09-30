29b9c497ecba882a07390ffc4958a49f45edb182
abwesendkr <manchmal2@abwesend.de>
2024-09-30 11:43:32 +0200

    missing powershellcommand added

--- install-packagemgr.ps1
+++ install-packagemgr.ps1
@@ -6,6 +6,9 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Write-Host "[FATAL] You need to run this script as Administrator! Exiting script." -ForegroundColor Red
     exit 1
 }
+#############
+# Functions #
+#############
 function Install-Chocolatey {
 
     if (Test-Path "C:\ProgramData\chocolatey") {
@@ -29,22 +32,6 @@ function Install-Chocolatey {
         Write-Host "Chocolatey is already installed." -ForegroundColor Yellow
     }
 }
-# Install Chocolatey if not already installed
-Install-Chocolatey
-
-# Refresh env
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-refreshenv
-
-# Install git
-$GitInstallCommand = "choco install git -y --no-progress"
-if ($env:TERM_PROGRAM -eq "vscode") {
-    $GitInstallCommand += " --noop"
-}
-Write-Host "Executing '$($GitInstallCommand)'"
-powershell.exe -Command $GitInstallCommand
-
-# Install Powershellmodule Winget
 function Install-Wingetpowershell {
     Write-Host "Attempting to install winget Powershellmodules Microsoft.WinGet.Client..."
 
@@ -78,3 +65,20 @@ function Install-Wingetpowershell {
         exit 1
     }
 }
+# Install Chocolatey if not already installed
+Install-Chocolatey
+
+# Refresh env
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+refreshenv
+
+# Install git
+$GitInstallCommand = "choco install git -y --no-progress"
+if ($env:TERM_PROGRAM -eq "vscode") {
+    $GitInstallCommand += " --noop"
+}
+Write-Host "Executing '$($GitInstallCommand)'"
+powershell.exe -Command $GitInstallCommand
+
+# Install Powershellmodule Winget
+Install-Wingetpowershell
