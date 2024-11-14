4e6c089b5594aa0c1e6d73fb6f56ef4290c40e04
abwesendkr <manchmal2@abwesend.de>
2024-11-14 16:09:41 +0100

    startup install kernelupdate

--- GPOs/Add-Startupscripts.ps1
+++ GPOs/Add-Startupscripts.ps1
@@ -19,5 +19,13 @@ Write-Host "Add $scriptPath to run each reboot"
 $taskAction = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -File $scriptPath"
 $taskTrigger = New-ScheduledTaskTrigger -AtStartup
 $taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-
 Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -Settings $taskSettings -TaskName "Add Admins to FSLogix Exclude List" -User "SYSTEM" -RunLevel Highest
+
+Write-Host "Add msiexec.exe /i c:\temp\wsl_update_x64.msi /quiet to run each reboot"
+
+# Create a scheduled task to run the script at startup
+$taskAction = New-ScheduledTaskAction -Execute "msiexec.exe" -Argument "/i c:\temp\wsl_update_x64.msi /quiet"
+$taskTrigger = New-ScheduledTaskTrigger -AtStartup
+$taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -Settings $taskSettings -TaskName "Install WSL-kernel-update on startup" -User "SYSTEM" -RunLevel Highest
+
