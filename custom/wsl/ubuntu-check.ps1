ea22beea502aea1495f36b5a3ec59bbca513227e
abwesendkr <manchmal2@abwesend.de>
2024-12-06 10:36:32 +0100

    added comments

--- custom/wsl/ubuntu-check.ps1
+++ custom/wsl/ubuntu-check.ps1
@@ -20,20 +20,20 @@ if ($check){
 else{ 
     Write-Host "Installing AppxPackage Ubuntu-22.04.appx"
     try {
-#        Start-Process -Wait Add-AppxPackage -Path C:\Users\Public\ubuntu-22.04.appx
+        # install via add-appxpackage predownloaded
         Start-Process -Wait -FilePath "powershell.exe" -ArgumentList "Add-AppxPackage -Path 'C:\Users\Public\ubuntu-22.04.appx'"
-# wiederholung von foreach da rein
+        # check if install successfull
         foreach ($item in $test) {
             # Remove all spaces and other whitespace characters
             $cleanedItem = -join ($item.ToCharArray() | Where-Object { $_ -ne ' ' -and $_ -ne [char]0 })
-        #    Write-Host "item: $item"
-        #    write-host "cleaneditem: $cleanedItem"
+            # search for distroname in output and if successfull start wsl.
             if ($cleanedItem -match $distro) {
                 Write-Output "Found $distro"
                 $check =$true
+                # start wsl
                 Write-host "Starting WSL"
                 Invoke-Expression "wsl.exe"
-            } else {
+            } else { # install via default wsl
                 Write-host "Appx Install failed, install ubuntu standard distro"
                 Invoke-Expression "wsl.exe --install -d Ubuntu"
                 Write-host "Starting WSL"
