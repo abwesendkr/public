fc5bee87caeb1ec47ac2c39884e683f0d33b2d6b
abwesendkr <manchmal2@abwesend.de>
2024-12-06 10:34:15 +0100

    error handeling wsl install

--- custom/wsl/ubuntu-check.ps1
+++ custom/wsl/ubuntu-check.ps1
@@ -22,6 +22,24 @@ else{
     try {
 #        Start-Process -Wait Add-AppxPackage -Path C:\Users\Public\ubuntu-22.04.appx
         Start-Process -Wait -FilePath "powershell.exe" -ArgumentList "Add-AppxPackage -Path 'C:\Users\Public\ubuntu-22.04.appx'"
+# wiederholung von foreach da rein
+        foreach ($item in $test) {
+            # Remove all spaces and other whitespace characters
+            $cleanedItem = -join ($item.ToCharArray() | Where-Object { $_ -ne ' ' -and $_ -ne [char]0 })
+        #    Write-Host "item: $item"
+        #    write-host "cleaneditem: $cleanedItem"
+            if ($cleanedItem -match $distro) {
+                Write-Output "Found $distro"
+                $check =$true
+                Write-host "Starting WSL"
+                Invoke-Expression "wsl.exe"
+            } else {
+                Write-host "Appx Install failed, install ubuntu standard distro"
+                Invoke-Expression "wsl.exe --install -d Ubuntu"
+                Write-host "Starting WSL"
+                Invoke-Expression "wsl.exe"    
+            }
+        }
     }
     catch {
         Write-host "install closed with: $_"
