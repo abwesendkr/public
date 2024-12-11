71c331b413346d9858781b25dd33eba7d8e97d51
abwesendkr <manchmal2@abwesend.de>
2024-12-11 17:21:55 +0100

    change startupscript

--- GPOs/Add-Startupscripts.ps1
+++ GPOs/Add-Startupscripts.ps1
@@ -13,19 +13,21 @@ catch {
     throw "Error: $_"
 }
 
-Write-Host "Add $scriptPath to run each reboot"
+#Write-Host "Add $scriptPath to run each reboot"
 
 # Create a scheduled task to run the script at startup
 $taskAction = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-ExecutionPolicy Bypass -File $scriptPath"
 $taskTrigger = New-ScheduledTaskTrigger -AtStartup
 $taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+Write-Host "[INFO] Add ""$($taskAction.Execute) $($taskAction.Arguments)"" to run each reboot"
 Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -Settings $taskSettings -TaskName "Add Admins to FSLogix Exclude List" -User "SYSTEM" -RunLevel Highest
-
-Write-Host "Add msiexec.exe /i c:\temp\wsl_update_x64.msi /quiet to run each reboot"
 
 # Create a scheduled task to run the script at startup
 $taskAction = New-ScheduledTaskAction -Execute "msiexec.exe" -Argument "/i c:\temp\wsl_update_x64.msi /quiet"
 $taskTrigger = New-ScheduledTaskTrigger -AtStartup
 $taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+Write-Host "[INFO] Add ""$($taskAction.Execute) $($taskAction.Arguments)"" to run each reboot"
 Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -Settings $taskSettings -TaskName "Install WSL-kernel-update on startup" -User "SYSTEM" -RunLevel Highest
+
+Write-Host "[INSTALLED] Scheduled Tasks"
 
