e71a38b3e42de42ab530cbdf582c7b89ee79c826
abwesendkr <manchmal2@abwesend.de>
2025-01-07 16:34:29 +0100

    updated to 1 file

--- UPDATE-to-newest-version.ps1
+++ UPDATE-to-newest-version.ps1
@@ -2,11 +2,13 @@
 #### INPUT ####
 ###############
 
+### run install-packagemgr before and open a new powershell!
 $Appjson = "apps-africa-single.json"
 
 ###############
 #### CODE #####
 ###############
+
 
 # Parse JSON file
 try {
@@ -16,6 +18,26 @@ catch {
     Write-Host "[FATAL] Failed to load apps.json, error message: $($_.Exception.Message)" -ForegroundColor Red
     exit 1
 }
+# CONSTANTS
+$REPO_NAME = "public"
+$GITHUB_REPO = "https://github.com/abwesendkr/public.git"
+
+# Check if running as Administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "[FATAL] You need to run this script as Administrator!Exiting script." -ForegroundColor Red
+    exit 1
+}
+
+# Load Git repo
+if (-not(Test-Path ".\$REPO_NAME")) {
+    & git clone $GITHUB_REPO
+}
+
+#Set work location
+Set-Location $REPO_NAME
+Write-Host "Cloned this:"
+tree /f
+
 Import-Module "./functions.psm1"
 
 # Set counters for successful installation
