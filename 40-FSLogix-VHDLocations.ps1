74a207e08521e353e926581be1e4698fba7f01a1
abwesendkr <manchmal2@abwesend.de>
2025-01-20 18:37:11 +0100

    created fslogix via vhd locations

---
+++ 40-FSLogix-VHDLocations.ps1
@@ -0,0 +1,21 @@
+#####################################
+# Install FSLogix with VHDLocations #
+#####################################
+
+# Import functions module
+Import-Module "./functions.psm1"
+
+# Read region from environment variable 
+Read-Region
+
+# Set test FSLogix 
+if($region = "test") {
+    $fslogix_regex_storageaccount = "random-stg-account"
+    $fslogix_regex_share = "random-share"
+}
+
+# Set FSLogix properties for vhdlocations
+$regPath = "HKLM:\SOFTWARE\FSLogix\Profiles"
+New-ItemProperty -Path $regPath -Name Enabled -PropertyType DWORD -Value 1 -Force
+New-ItemProperty -Path $regPath -Name VHDLocations -PropertyType MultiString -Value "\\$fslogix_regex_storageaccount.file.core.windows.net\$fslogix_regex_share" -Force
+\\crmecupaes01fxst001.privatelink.file.core.windows.net\singlessession
