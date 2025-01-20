948dac83bf380e87ba83c8ab978998298e0f80fd
abwesendkr <manchmal2@abwesend.de>
2025-01-20 20:16:31 +0100

    set variables

--- 40-FSLogix-VHDLocations.ps1
+++ 40-FSLogix-VHDLocations.ps1
@@ -13,12 +13,12 @@ $region = Read-Region
 # Set test FSLogix 
 if($region = "test") {
     Write-Host "[INFO] set fslogix variables"
-    $fslogix_regex_storageaccount = "random-stg-account"
-    $fslogix_regex_share = "random-share"
+    $fslogix_regex_storageaccount = "crmecupaes01fxst001"
+    $fslogix_regex_share = "singlessession"
 }
 
 # Set FSLogix properties for vhdlocations
 $regPath = "HKLM:\SOFTWARE\FSLogix\Profiles"
 New-ItemProperty -Path $regPath -Name Enabled -PropertyType DWORD -Value 1 -Force
-New-ItemProperty -Path $regPath -Name VHDLocations -PropertyType MultiString -Value "\\$fslogix_regex_storageaccount.file.core.windows.net\$fslogix_regex_share" -Force
+New-ItemProperty -Path $regPath -Name VHDLocations -PropertyType MultiString -Value "\\$fslogix_regex_storageaccount.privatelink.file.core.windows.net\$fslogix_regex_share" -Force
 \\crmecupaes01fxst001.privatelink.file.core.windows.net\singlessession
