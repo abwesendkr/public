b134a80c1af8a034f50aa185edc21bb5b7d29324
abwesendkr <manchmal2@abwesend.de>
2025-02-05 09:09:35 +0100

    region control and change do custom apps instead of permanent

--- 40-FSLogix-VHDLocations.ps1
+++ 40-FSLogix-VHDLocations.ps1
@@ -32,22 +32,26 @@ Write-Host "[INFO] Set share to: $($fslogix_regex_storageaccount) and $($fslogix
 
 # Set FSLogix properties for vhdlocations
 Write-Host "[INFO] try set registry "
-try {
-    $regPath = "HKLM:\SOFTWARE\FSLogix\Profiles"
-    New-ItemProperty -Path $regPath -Name Enabled -PropertyType DWORD -Value 1 -Force
-    New-ItemProperty -Path $regPath -Name VHDLocations -PropertyType MultiString -Value "\\$fslogix_regex_storageaccount.privatelink.file.core.windows.net\$fslogix_regex_share" -Force
-    New-ItemProperty -Path $regPath -Name SizeInMBs -PropertyType DWORD -Value 30000 -Force
-    New-ItemProperty -Path $regPath -Name IsDynamic -PropertyType DWORD -Value 1 -Force
-    New-ItemProperty -Path $regPath -Name VolumeType -PropertyType String -Value "vhdx" -Force
-    New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force
-    New-ItemProperty -Path $regPath -Name DeleteLocalProfileWhenVHDShouldApply -PropertyType DWORD -Value 1 -Force
-    New-ItemProperty -Path $regPath -Name "PreventLoginWithFailure" -PropertyType DWORD -Value 1 -Force
-    New-ItemProperty -Path $regPath -Name "PreventLoginWithTempProfile" -PropertyType DWORD -Value 1 -Force
-    Write-Host "[INSTALLED] Registriy set"
-}
-catch {
-    Write-Host "[ERROR] Error installing $($App): $_"
-    throw $_
+if($region) {
+    try {
+        $regPath = "HKLM:\SOFTWARE\FSLogix\Profiles"
+        New-ItemProperty -Path $regPath -Name Enabled -PropertyType DWORD -Value 1 -Force
+        New-ItemProperty -Path $regPath -Name VHDLocations -PropertyType MultiString -Value "\\$fslogix_regex_storageaccount.privatelink.file.core.windows.net\$fslogix_regex_share" -Force
+        New-ItemProperty -Path $regPath -Name SizeInMBs -PropertyType DWORD -Value 30000 -Force
+        New-ItemProperty -Path $regPath -Name IsDynamic -PropertyType DWORD -Value 1 -Force
+        New-ItemProperty -Path $regPath -Name VolumeType -PropertyType String -Value "vhdx" -Force
+        New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force
+        New-ItemProperty -Path $regPath -Name DeleteLocalProfileWhenVHDShouldApply -PropertyType DWORD -Value 1 -Force
+        New-ItemProperty -Path $regPath -Name "PreventLoginWithFailure" -PropertyType DWORD -Value 1 -Force
+        New-ItemProperty -Path $regPath -Name "PreventLoginWithTempProfile" -PropertyType DWORD -Value 1 -Force
+        Write-Host "[INSTALLED] Registriy set"
+    }
+    catch {
+        Write-Host "[ERROR] Error installing $($App): $_"
+        throw $_
+    }
+} else {
+    Write-Host "[ERROR] Region not set"
 }
 
 
