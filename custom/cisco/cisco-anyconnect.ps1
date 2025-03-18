de75051f53d67af481972c717d453f6065262498
abwesendkr <manchmal2@abwesend.de>
2025-03-18 11:39:34 +0100

    wrong paths

--- custom/cisco/cisco-anyconnect.ps1
+++ custom/cisco/cisco-anyconnect.ps1
@@ -1,7 +1,7 @@
 # Skript Pfad: C:\Scripts\Install-SQLDeveloper.ps1
 
 $App = "Cisco Anyconnect"
-$MSIFile = "anyconnect-win-4.10.08029-core-vpn-predeploy-k9.msi"
+$MSIFile = Join-Path -path $PSScriptRoot -ChildPath "anyconnect-win-4.10.08029-core-vpn-predeploy-k9.msi"
 $Switches = "REBOOT=ReallySuppress /QN"
 
 # Überprüfen, ob das ZIP-File existiert
@@ -12,8 +12,8 @@ if (-Not (Test-Path -Path $MSIFile)) {
 # ZIP-Datei entpacken
 try {
     Write-Host "[INFO] Install $MSIFile"
-    Start-Process -Wait -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i", "$MsiPath", "$Switches"
-    Copy-File -Path "preferences_global.xml" -Destination "$envProgramdata\Cisco\Cisco AnyConnect Secure Mobility Client"
+    Start-Process -Wait -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i", "$MSIFile", "$Switches"
+    Copy-item -Path "$psscriptroot\preferences_global.xml" -Destination "$env:Programdata\Cisco\Cisco AnyConnect Secure Mobility Client"
     # Erfolgreiches Ende
     Write-Host " $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Installation $App erfolgreich abgeschlossen."
 } catch {
